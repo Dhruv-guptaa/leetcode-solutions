044ed94e35c5baa4be071a01eeaca1576ed5b6ed
Dhruv Gupta <dhruvedition@gmail.com>
2026-01-21 12:12:53 +0530

    Time: 35 ms (36.39%) | Memory: 14.1 MB (66.90%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,20 @@
+//problem number : 001
+// Two Sum
+// method nested loop
+// complexity O(n^2)
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};  // return vector with indices
+                }
+            }
+        }
+        return {}; // in case no solution is found
+    }
+};
